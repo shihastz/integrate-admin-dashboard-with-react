679d52eddf9d76538e9c9f52b9cb6432ef7c92bf
shihastz <shihast0503@gmail.com>
2021-05-20 22:32:48 +0530

    latest changes

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="main-footer">
+        <strong>2014-2021 
+        </strong>
+         All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 1.1.0
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
